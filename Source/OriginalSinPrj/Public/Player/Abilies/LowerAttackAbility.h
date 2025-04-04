5c04a8707762b9d8c1ef87b7cecf52849b309cbe
juchoi0632 <juchoi0632@gmail.com>
2025-04-04 09:51:31 +0900

    add abilities c++ class

---
+++ Source/OriginalSinPrj/Public/Player/Abilies/LowerAttackAbility.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/Abilies/BaseWitchAbility.h"
+#include "LowerAttackAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORIGINALSINPRJ_API ALowerAttackAbility : public ABaseWitchAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/OriginalSinPrj/Public/Player/Abilies/Skill3Ability.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/Abilies/BaseWitchAbility.h"
+#include "Skill3Ability.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORIGINALSINPRJ_API ASkill3Ability : public ABaseWitchAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
