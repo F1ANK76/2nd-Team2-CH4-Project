9629021af9321f981c49788d82f5e87fb0d437f8
HyeonjungYun <guswnd9971@gmail.com>
2025-04-11 19:49:13 +0900

    Update Platform스폰, 삭제 로직 추가 Add 액터 삭제하는 데스존 영역 추가

--- Source/OriginalSinPrj/Public/LevelObject/BasePlatform.h
+++ Source/OriginalSinPrj/Public/LevelObject/BasePlatform.h
@@ -2,33 +2,34 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "TestPlatform.generated.h"
+#include "BasePlatform.generated.h"
 
 class UBoxComponent;
 
 UCLASS()
-class ORIGINALSINPRJ_API ATestPlatform : public AActor
+class ORIGINALSINPRJ_API ABasePlatform : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	ATestPlatform();
+	ABasePlatform();
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-public:	
+public:
 	virtual void Tick(float DeltaTime) override;
 
 public:
 
-	UPROPERTY(EditAnywhere, Replicated ,Category = "TestPlatform")
+	UPROPERTY(EditAnywhere, Replicated, Category = "TestPlatform")
 	USceneComponent* Scene;
 
-	UPROPERTY(EditAnywhere, Replicated ,Category = "TestPlatform")
+	UPROPERTY(EditAnywhere, Replicated, Category = "TestPlatform")
 	UBoxComponent* Box;
-	
-	UPROPERTY(EditAnywhere, Replicated ,Category = "TestPlatform")
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "TestPlatform")
 	UStaticMeshComponent* StaticMesh;
+
 };
