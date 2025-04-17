06d1cd82e514d6840008b2e7fa850ee68a4a93fe
F1ANK <ygodlksg@gmail.com>
2025-04-17 09:33:09 +0900

    싱글플레이 데스존 처리 제외 까지 로직 추가

--- Source/OriginalSinPrj/Private/SpawnManager.cpp
+++ Source/OriginalSinPrj/Private/SpawnManager.cpp
@@ -51,7 +51,7 @@ void ASpawnManager::SpawnMonster(AController* Controller, FVector SpawnLocation)
 		SpawnParams.Owner = Controller;
 
 		APawn* NewMonster = GetWorld()->SpawnActor<APawn>(
-			PlayerClass,
+			MonsterClass,
 			SpawnLocation,
 			FRotator::ZeroRotator,
 			SpawnParams
